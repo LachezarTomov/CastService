03207ee9f6f62de74bf2ceeed56fb9e5d1937b05
LachezarTomov <lbtomov@gmail.com>
2014-12-02 21:56:21 +0200

    Added AuditInfo.cs, IDeletableEntity.cs and migrations

---
+++ CastService/CastService.Data.Common/Models/IDeletableEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace CastService.Data.Common.Models
+{
+    using System;
+
+    public interface IDeletableEntity
+    {
+        bool IsDeleted { get; set; }
+
+        DateTime? DeletedOn { get; set; }
+    }
+}
--- CastService/Data/CastService.Data/ApplicationDbContext.cs
+++ CastService/Data/CastService.Data/ApplicationDbContext.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
 
 using Microsoft.AspNet.Identity.EntityFramework;
 
 using CastService.Data.Models;
+using CastService.Data.Migrations;
 
 
 namespace CastService.Data
@@ -12,6 +15,7 @@ namespace CastService.Data
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
         }
 
         public static ApplicationDbContext Create()
